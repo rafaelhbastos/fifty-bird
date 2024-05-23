daa6c98d3aa21374501b4e59cc487933b94e8ecf
rafaelhbastos <rafaelhenribastos12@gmail.com>
2024-05-22 23:11:49 -0300

    fifty-bird version 5

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ Class = require 'class'
 
 require 'Bird'
 
+require 'Pipe'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -22,6 +24,10 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
+
+local pipes = {}
+
+local spawnTimer = 0
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -57,7 +63,23 @@ function love.update(dt)
 
   groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
+  spawnTimer = spawnTimer + dt
+
+  if spawnTimer > 2 then
+    table.insert(pipes, Pipe())
+    print('Added new pipe!')
+    spawnTimer = 0
+  end
+
   bird:update(dt)
+
+  for k, pipe in pairs(pipes) do
+    pipe:update(dt)
+
+    if pipe.x < -pipe.width then
+      table.remove(pipes, k)
+    end
+  end
 
   love.keyboard.keysPressed = {}
 end
@@ -66,6 +88,10 @@ function love.draw()
   push:start()
 
   love.graphics.draw(background, -backgroundScroll, 0)
+
+  for k, pipe in pairs(pipes) do
+    pipe:render()
+  end
 
   love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
