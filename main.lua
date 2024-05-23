5578dbf4b7ba5b9fdb9bf27f5f2a2eb4e43dd505
rafaelhbastos <rafaelhenribastos12@gmail.com>
2024-05-22 22:41:56 -0300

    fifty-bird version 3

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 push = require 'push'
 
+Class = require 'class'
+
+require 'Bird'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -16,6 +20,8 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -43,6 +49,8 @@ function love.update(dt)
   backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 
   groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+  bird:update(dt)
 end
 
 function love.draw()
@@ -51,6 +59,8 @@ function love.draw()
   love.graphics.draw(background, -backgroundScroll, 0)
 
   love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+  bird:render()
 
   push:finish()
 end
